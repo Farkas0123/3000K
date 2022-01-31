a606d8097bf74fe85944da62a3719af16466d163
Farkas Ükös <ukosfarkas@gmail.com>
2022-01-31 11:00:26 +0100

    elso kesz

--- Farkas.py
+++ Farkas.py
@@ -1,1 +1,13 @@
-from main import Tanulo
+from main import Csapat
+
+def feladat1(lista):
+    print("1)  Írja ki Magyarország által elért helyezéseket. A kiírásban jelenjen meg a vb éve és helyszíne is.")
+    vissza = list(filter(lambda cs: cs.csapat=="Magyarország", lista))
+    for sor in vissza:
+        print(f"Magyarország a(z) {sor.helyezes}-dik helyen végzett az {sor.ev} évi világbajnokságon, amit {sor.vbhely} szervezett")
+    print()
+
+
+
+
+feladat1(Csapat.lista)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class Tanulo:
+class Csapat:
     lista = []
     def __init__(self, sorzsam, csapat, helyezes, ev, vbhely):
         self.sorszam = sorzsam
@@ -6,9 +6,9 @@ class Tanulo:
         self.helyezes = helyezes
         self.ev = ev
         self.vbhely = vbhely
-        Tanulo.lista.append(self)
+        Csapat.lista.append(self)
 
 with open("input.txt", "r", encoding="utf8") as f:
     for sor in f:
         s = sor.strip().split("\t")
-        t = Tanulo(int(s[0]), s[1], int(s[2]), int(s[3]), s[4])
+        t = Csapat(int(s[0]), s[1], int(s[2]), int(s[3]), s[4])
