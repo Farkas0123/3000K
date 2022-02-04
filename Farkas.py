a66b675ec0a82353957bbd2fac35ada43c2f4dd2
Farkas Ükös <ukosfarkas@gmail.com>
2022-02-04 12:49:20 +0100

    Valamit alkottam

--- Farkas.py
+++ Farkas.py
@@ -59,6 +59,49 @@ def feladat23(lista):
     print(kinyert[0].csapat if len(kinyert) == 1 else "Ekkor nem rendeztek vb-t!")
     print()
 
+def feladat24(lista):
+    print("24)	Hányszor kapott ki a döntőben Magyarország?")
+    print(len(list(filter(lambda cs: cs.helyezes == 2 and cs.csapat == "Magyarország", lista))))
+    print()
+
+def feladat30(lista):
+    print("30)	A programm olvasson be évszámot és írja ki, hogy melyik csapat nyert az adott évben?")
+    a = int(input())
+    b = list(filter(lambda cs: cs.ev == a and cs.helyezes == 1, lista))
+    print(b[0].csapat if len(b) == 1 else "Ebben az évben nem rendeztek vb-t!")
+
+def feladat60(lista):
+    print("60)	Melyik csapat(ok) nyert a legtöbbször vb-t? A csapat neve mellett a vb gyözelmmek számát is írja ki! ")
+    a  = list(filter(lambda cs: cs.helyezes == 1, lista))
+    b = ""
+    d = ""
+    max = 0
+    akt = 0
+    i = 0
+    for sor in a:
+        b = a[0].csapat
+        print(b)
+        while i < len(a):
+            #print(b+"...")
+            if a[i].csapat == b:
+                a.remove(a[i])
+                akt += 1
+            i += 1
+        
+        if akt > max:
+            d = b + " " + str(akt)
+            max = akt
+        elif akt == max:
+            d += ", " + b + " " + str(akt)
+        akt = 0
+        b = ""
+        i = 0
+    print(d)
+    print()
+
+
+
+
 
 feladat1(Csapat.lista)
 #feladat6(Csapat.lista)
@@ -69,3 +112,6 @@ feladat20(Csapat.lista)
 feladat21(Csapat.lista)
 feladat22(Csapat.lista)
 feladat23(Csapat.lista)
+feladat24(Csapat.lista)
+#feladat30(Csapat.lista)
+feladat60(Csapat.lista)
